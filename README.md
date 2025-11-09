fd8afa431f5b0125cf1d8c58c08803075870192c
Bernardo Souza <bernardo.sfs27@gmail.com>
2025-11-09 17:41:01 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+![Capa do Repositório](<Capa -  Web.png>)
+
+# My Studies - Web
+
+Projeto para implementar o front-end da API desenvolvida anteriormente em .NET .
+
+- **API:** https://github.com/BernardoS/mystudies
+
+---
+
+## Propósito
+
+O propósito deste projeto é desenvolver uma aplicação front-end React completa que interaja com uma API construída em .NET .
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import myStudiesLogo from './assets/logo-centered.png'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='in-construction-div'>
+      <img height={150} alt='My Studies' src={myStudiesLogo}/>
+      <p>Projeto criado para organizar notas de estudo por assuntos revisá-los através de flash cards personalizados.</p>
+    </div>
+  )
+}
+
+export default App
